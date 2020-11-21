a2a1e0554da9c5ab7ea78e1798430a2f474bf704
Ukesh Gaiju <ukesh2010@gmail.com>
2020-11-21 12:11:57 +0545

    scss added

---
+++ pages/home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+import "./index.module.scss";
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <div className="test">welcome to creation arts</div>
+  </div>
+);
+
+export default Home;
