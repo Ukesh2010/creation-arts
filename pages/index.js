358900a952ccac815eaaff7e7bf91521861d6c21
Ukesh Gaiju <ukesh2010@gmail.com>
2021-03-21 13:55:22 +0545

    featured products and new arival displayed

--- pages/index.js
+++ pages/index.js
@@ -11,172 +11,138 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import { getServerSideCookie } from "../utils/serverSideStorage";
+import { getProducts } from "../api";
+import { PRODUCT_IMAGE_FILLER } from "../utils/consts";
 
-const Home = () => (
-  <>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Nav />
-    <section className="home-container">
-      <div className="banner">
-        <div className="banner-image">
-          <Image src={"/images/bg.jpeg"} alt={"banner image"} layout={"fill"} />
-        </div>
+const Home = (props) => {
+  const { featuredProducts, newProducts } = props;
 
-        <div className="social-icons">
-          <div className="icon-button">
-            <FontAwesomeIcon icon={faFacebookF} size={"2x"} />
+  console.log("props", props);
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Nav />
+      <section className="home-container">
+        <div className="banner">
+          <div className="banner-image">
+            <Image
+              src={"/images/bg.jpeg"}
+              alt={"banner image"}
+              layout={"fill"}
+            />
           </div>
-          <div className="icon-button">
-            <FontAwesomeIcon icon={faInstagram} size={"2x"} />
-          </div>
-          <div className="icon-button">
-            <FontAwesomeIcon icon={faTwitter} size={"2x"} />
-          </div>
-        </div>
-        <div className="control-icons">
-          <div className="icon-button arrow-up">
-            <FontAwesomeIcon icon={faArrowUp} size={"2x"} />
-          </div>
-          <div className="icon-button arrow-down">
-            <FontAwesomeIcon icon={faArrowDown} size={"2x"} />
-          </div>
-        </div>
 
-        <div className="banner-details">
-          <h3 className="title">Hand Crafted Products</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
-            atque consequatur cumque doloribus ducimus fugiat inventore,
-            molestias neque nihil perspiciatis, praesentium quae, quibusdam
-            repellat ut vel? Accusantium fuga in ipsum?
-          </p>
-          <Link href={`/products`}>
-            <button className="btn accent-btn">Shop Now</button>
-          </Link>
+          <div className="social-icons">
+            <div className="icon-button">
+              <FontAwesomeIcon icon={faFacebookF} size={"2x"} />
+            </div>
+            <div className="icon-button">
+              <FontAwesomeIcon icon={faInstagram} size={"2x"} />
+            </div>
+            <div className="icon-button">
+              <FontAwesomeIcon icon={faTwitter} size={"2x"} />
+            </div>
+          </div>
+          <div className="control-icons">
+            <div className="icon-button arrow-up">
+              <FontAwesomeIcon icon={faArrowUp} size={"2x"} />
+            </div>
+            <div className="icon-button arrow-down">
+              <FontAwesomeIcon icon={faArrowDown} size={"2x"} />
+            </div>
+          </div>
+
+          <div className="banner-details">
+            <h3 className="title">Hand Crafted Products</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+              atque consequatur cumque doloribus ducimus fugiat inventore,
+              molestias neque nihil perspiciatis, praesentium quae, quibusdam
+              repellat ut vel? Accusantium fuga in ipsum?
+            </p>
+            <Link href={`/products`}>
+              <button className="btn accent-btn">Shop Now</button>
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="container">
-        <div className="featured-container">
-          <div className="heading">Featured Shop</div>
-          <div className="product-container">
-            <div className="product-item">
-              <div className="product-image">
-                <Image
-                  src="/images/product1.jpg"
-                  alt="Picture of the author"
-                  layout="fill"
-                />
-              </div>
-              <div className="item-detail">
-                <div className="name">Title</div>
-                <div className="price">$50</div>
-              </div>
+        <div className="container">
+          <div className="featured-container">
+            <div className="heading">Featured products</div>
+            <div className="product-container">
+              {featuredProducts.map((item, index) => {
+                const [image] = item?.images || [];
+                return (
+                  <Link href={`/product-detail/${item?._id}`} key={index}>
+                    <div className="product-item">
+                      <div className="product-image">
+                        <Image
+                          src={image?.url || PRODUCT_IMAGE_FILLER}
+                          alt={image?.originalFileName || "Product image"}
+                          layout="fill"
+                        />
+                      </div>
+                      <div className="item-detail">
+                        <div className="name">{item.name}</div>
+                        <div className="price">${item.price}</div>
+                      </div>
+                    </div>
+                  </Link>
+                );
+              })}
             </div>
-            <div className="product-item">
-              <div className="product-image">
-                <Image
-                  src="/images/product1.jpg"
-                  alt="Picture of the author"
-                  layout="fill"
-                />
-              </div>
-              <div className="item-detail">
-                <div className="name">Title</div>
-                <div className="price">$50</div>
-              </div>
-            </div>
-            <div className="product-item">
-              <div className="product-image">
-                <Image
-                  src="/images/product2.jpg"
-                  alt="Picture of the author"
-                  layout="fill"
-                />
-              </div>
-              <div className="item-detail">
-                <div className="name">Title</div>
-                <div className="price">$50</div>
-              </div>
-            </div>
-            <div className="product-item">
-              <div className="product-image">
-                <Image
-                  src="/images/product2.jpg"
-                  alt="Picture of the author"
-                  layout="fill"
-                />
-              </div>
-              <div className="item-detail">
-                <div className="name">Title</div>
-                <div className="price">$50</div>
-              </div>
+          </div>
+          <div className="new-arrivals-container">
+            <div className="heading">New Arrivals</div>
+            <div className="product-container">
+              {newProducts.map((item, index) => {
+                const [image] = item?.images || [];
+                return (
+                  <Link href={`/product-detail/${item?._id}`} key={index}>
+                    <div className="product-item">
+                      <div className="product-image">
+                        <Image
+                          src={image?.url || PRODUCT_IMAGE_FILLER}
+                          alt={image?.originalFileName || "Product image"}
+                          layout="fill"
+                        />
+                      </div>
+                      <div className="item-detail">
+                        <div className="name">{item.name}</div>
+                        <div className="price">${item.price}</div>
+                      </div>
+                    </div>
+                  </Link>
+                );
+              })}
             </div>
           </div>
         </div>
-        <div className="new-arrivals-container">
-          <div className="heading">New Arrivals</div>
-          <div className="product-container">
-            <div className="product-item">
-              <div className="product-image">
-                <Image
-                  src="/images/product1.jpg"
-                  alt="Picture of the author"
-                  layout="fill"
-                />
-              </div>
-              <div className="item-detail">
-                <div className="name">Title</div>
-                <div className="price">$50</div>
-              </div>
-            </div>
-            <div className="product-item">
-              <div className="product-image">
-                <Image
-                  src="/images/product1.jpg"
-                  alt="Picture of the author"
-                  layout="fill"
-                />
-              </div>
-              <div className="item-detail">
-                <div className="name">Title</div>
-                <div className="price">$50</div>
-              </div>
-            </div>
-            <div className="product-item">
-              <div className="product-image">
-                <Image
-                  src="/images/product1.jpg"
-                  alt="Picture of the author"
-                  layout="fill"
-                />
-              </div>
-              <div className="item-detail">
-                <div className="name">Title</div>
-                <div className="price">$50</div>
-              </div>
-            </div>
-            <div className="product-item">
-              <div className="product-image">
-                <Image
-                  src="/images/product1.jpg"
-                  alt="Picture of the author"
-                  layout="fill"
-                />
-              </div>
-              <div className="item-detail">
-                <div className="name">Title</div>
-                <div className="price">$50</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <Footer />
-  </>
-);
+      </section>
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
+
+export const getServerSideProps = async (context) => {
+  const token = getServerSideCookie(context)("token");
+
+  try {
+    const featuredProducts = await getProducts(token)({
+      isFeatured: true,
+      limit: 8,
+    });
+    const newProducts = await getProducts(token)({
+      limit: 8,
+    });
+    return { props: { featuredProducts, newProducts } };
+  } catch (e) {
+    console.log("error", e.message);
+  }
+  return { props: { featuredProducts: [], newProducts: [] } };
+};
